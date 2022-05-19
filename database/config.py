e3990cf7bd599442de2740e60f53394cdf4d4ecd
Madashy <75827242+Madashy@users.noreply.github.com>
2022-05-19 10:01:53 -0300

    Updated gitignore and database config file
    Added test.db to gitignore and changed the SQL database URL to create a sqlite .db in the database folder

--- database/config.py
+++ database/config.py
@@ -2,7 +2,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///database/fastcrudapp.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./database/fastcrudapp.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, future=True, echo=True
